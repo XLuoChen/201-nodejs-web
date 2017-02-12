e5e0050ffd76c79befde9a51e6abf211c01dfa20
XLuoChen <1425145741@qq.com>
2017-02-12 15:44:18 +0800

    change Item model data structure

--- controller/ItemController.js
+++ controller/ItemController.js
@@ -1,73 +1,73 @@
 const Item = require('../models/item');
 
 export default class ItemController {
-    getAll(req, res, next) {
-        Item.find({}, (err, result) => {
-            if (err) {
-                return next(err);
-            } else {
-                res.send(result);
-            }
-        });
-    }
+  getAll(req, res, next) {
+    Item.find({}, (err, result) => {
+      if (err) {
+        return next(err);
+      } else {
+        res.send(result);
+      }
+    });
+  }
 
-    getOne(req, res, next) {
-        const newItem = new Item();
-        newItem.itemId = req.params.itemId;
-        Item.find({itemId: newItem.itemId}, (err, result) => {
-            if (err) {
-                return next(err);
-            } else {
-                res.send(result);
-            }
-        })
-    }
+  getOne(req, res, next) {
+    const newItem = new Item();
+    newItem.itemId = req.params.itemId;
+    Item.find({'_id': newItem.itemId}, (err, result) => {
+      if (err) {
+        return next(err);
+      } else {
+        res.send(result);
+      }
+    })
+  }
 
-    saveItem(req, res, next) {
-        const newItem = new Item();
-        newItem.itemId = req.body.itemId;
-        newItem.name = req.body.name;
-        newItem.category = req.body.category;
+  saveItem(req, res, next) {
+    const newItem = new Item();
+    newItem.itemId = req.body.itemId;
+    newItem.name = req.body.name;
+    newItem.category = req.body.category;
 
-        Item.create({itemId: newItem.itemId, name: newItem.name, category: newItem.category}, (err, result) => {
-            if (err) {
-                next(err);
-            } else {
-                res.send('ok');
-            }
-        })
-    }
+    Item.create({name: newItem.name, category: newItem.category}, (err, result) => {
+      if (err) {
+        next(err);
+      } else {
+        res.send('ok');
+      }
+    })
+  }
 
-    deleteItem(req, res, next) {
-        const newItem = new Item();
-        newItem.itemId = req.params.itemId;
+  deleteItem(req, res, next) {
+    const newItem = new Item();
+    newItem.itemId = req.params.itemId;
 
-        Item.remove({itemId: newItem.itemId}, (err, result) => {
-            if (err) {
-                next(err);
-            } else {
-                res.send('ok');
-            }
-        })
-    }
+    Item.remove({'_id': newItem.itemId}, (err, result) => {
+      if (err) {
+        next(err);
+      } else {
+        res.send('ok');
+      }
+    })
+  }
 
-    modifyItem(req, res, next) {
-        const newItem = new Item();
-        newItem.itemId = req.params.itemId;
-        newItem.name = req.body.name;
-        newItem.category = req.body.category;
+  modifyItem(req, res, next) {
+    const newItem = new Item();
+    newItem.itemId = req.params.itemId;
+    newItem.name = req.body.name;
+    newItem.category = req.body.category;
 
-        Item.update({itemId: newItem.itemId}, {
-            $set: {
-                name: newItem.name,
-                category: newItem.category
-            }
-        }, (err, result) => {
-            if (err) {
-                next(err);
-            } else {
-                res.send(result);
-            }
-        })
-    }
+    Item.update({'_id': newItem.itemId}, {
+      $set: {
+        name: newItem.name,
+        category: newItem.category
+      }
+    }, (err, result) => {
+      if (err) {
+        next(err);
+      } else {
+        res.send(result);
+      }
+    })
+  }
 }
--- models/item.js
+++ models/item.js
@@ -3,6 +3,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ItemSchema = new Schema({itemId: Number, name: String, category: String});
+const ItemSchema = new Schema({name: String, category: String});
 
 module.exports = mongoose.model('Item', ItemSchema);
