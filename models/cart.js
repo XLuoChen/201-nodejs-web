864e4c2dd0468900a472f3184f4fcbd83cffdb95
XLuoChen <1425145741@qq.com>
2017-02-14 12:16:05 +0800

    add refreshMongo and fixture

--- models/cart.js
+++ models/cart.js
@@ -3,6 +3,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const cartSchema = new Schema({items: Array});
+const cartSchema = new Schema({
+  uerId: String,
+  items: [{
+    item: {
+      type: Schema.ObjectId,
+      ref: 'Item'
+    },
+    count: Number
+  }]
+});
 
 module.exports = mongoose.model('Cart', cartSchema);
---
+++ tool/refreshMongo.js
@@ -0,0 +1,31 @@
+const mogoose = require('mongoose');
+const rawData = require('./fixture/raw-data');
+const Item = require('../models/item');
+const Category = require('../models/category');
+const Cart = require('../models/cart');
+
+const modelMap = {
+  Item,
+  Cart,
+  Category
+};
+
+let docs = Object.keys(rawData);
+
+// mogoose.connect('mongodb://localhost/supermarket');
+module.exports = function refresh(done) {
+
+  Object.keys(rawData).forEach((v) => {
+    modelMap[v].remove(() => {
+      modelMap[v].create(rawData[v], () => {
+        docs = docs.filter(doc => doc !== v);
+        if (docs.length === 0) {
+          done
+          // console.log('refreshMongo success')
+          // process.exit(0);
+        }
+      })
+    });
+  });
+
+};
