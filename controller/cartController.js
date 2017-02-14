793d65fd81d31513ed93a23e53c0005d18aaccf4
XLuoChen <1425145741@qq.com>
2017-02-14 14:56:31 +0800

    add spec for cartController

--- controller/cartController.js
+++ controller/cartController.js
@@ -1,49 +1,77 @@
 const Cart = require('../models/cart');
+const async = require('async');
+
+const loadItemUri = function (items) {
+  return items.map(({count, item}) => {
+    return {uri: `items/${item}`, count};
+  });
+};
 
 class CartController {
   getAll(req, res, next) {
-    Cart.find({}, (err, result) => {
+    async.series({
+      items: (cb) => {
+        Cart.find({}, (err, doc) => {
+          if (err) {
+            return next(err);
+          }
+          const carts = doc.map(item => {
+            return item.toJSON();
+          });
+          cb(null, carts);
+        })
+      },
+      totalCount: (cb) => {
+        Cart.count(cb);
+      }
+    }, (err, result) => {
       if (err) {
         return next(err);
-      } else {
-        res.json(result);
       }
+      return res.status(200).send(result);
     })
   }
 
   getOne(req, res, next) {
     const cartId = req.params.cartId;
 
-    Cart.find({cartId}, (err, result) => {
+    Cart.findById(cartId, (err, doc) => {
       if (err) {
         return next(err);
-      } else {
-        res.json(result);
+      }
+      if (!doc) {
+        return res.sendStatus(404);
+      }
+      else {
+        const data = doc.toJSON();
+        data.items = loadItemUri(data.items);
+        return res.status(200).send(data);
       }
     })
   }
 
   saveCart(req, res, next) {
-    const cart = new Cart();
-    cart.cartId = req.body.cartId;
-    cart.items = req.body.items;
 
-    Cart.create(cart, (err, result) => {
+    Cart.create(req.body, (err, result) => {
       if (err) {
         return next(err);
       } else {
-        res.json(result);
+        return res.status(201).send({uri: `carts/${result._id}`});
       }
     });
   }
 
   deleteCart(req, res, next) {
     const cartId = req.params.cartId;
-    Cart.remove({cartId}, (err, result) => {
+    Cart.findOneAndRemove({'_id': cartId}, (err, result) => {
       if (err) {
         return next(err);
-      } else {
-        res.send('deleted');
+      }
+      if (!result) {
+        return res.sendStatus(404);
+      }
+      else {
+        return res.sendStatus(204);
       }
     });
   }
@@ -51,11 +79,15 @@ class CartController {
   modifyCart(req, res, next) {
     const cartId = req.params.cartId;
 
-    Cart.update({cartId}, {$set: {items: req.body.items}}, (err, result) => {
+    Cart.update({'_id': cartId}, {$set: req.body}, (err, result) => {
       if (err) {
         return next(err);
-      } else {
-        res.send('modified successfully!');
+      }
+      if (!result) {
+        return res.sendStatus(404);
+      }
+      else {
+        return res.sendStatus(204);
       }
     });
   }
--- spec/cart-spec.js
+++ spec/cart-spec.js
@@ -1,3 +1,95 @@
-/**
- * Created by never on 2/14/17.
- */
+require('should');
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+
+const refresh = require('../tool/refreshMongo');
+const Cart = require('../models/cart');
+
+describe('CartController', () => {
+  beforeEach(() => {
+    refresh();
+  });
+
+  it('/GET /carts', (done) => {
+    request
+      .get('/carts')
+      .expect(200)
+      .expect((res) => {
+        res.body.totalCount.should.equal(1);
+        res.body.items.length.should.equal(1);
+      })
+      .end(done);
+  });
+
+  it('GET /carts/:cartId', (done) => {
+    request
+      .get('/carts/587f0f2586653d19297d40c6')
+      .expect(200)
+      .expect((res) => {
+        res.body.should.eql({
+          "_id": "587f0f2586653d19297d40c6",
+          "__v": 0,
+          "items": [{
+            "uri": "items/587f0f2586653d19297d40c2",
+            "count": 1
+          }, {
+            "uri": "items/587f0f2586653d19297d40c3",
+            "count": 1
+          }, {
+            "uri": "items/587f0f2586653d19297d40c4",
+            "count": 1
+          }]
+        });
+      })
+      .end(done);
+  });
+
+  it('POST /carts', (done) => {
+    const cart = {
+      userId: '2',
+      items: [
+        {
+          count: 4,
+          item: '587f0f2586653d19297d40c2'
+        }
+      ]
+    };
+
+    request
+      .post('/carts')
+      .send(cart)
+      .expect(201)
+      .expect((res) => {
+        Cart.findOne(cart, (err, doc) => {
+          res.body.uri.should.equal(doc._id);
+        });
+      })
+      .end(done);
+  });
+
+  it('DELETE /carts/:cartId', (done) => {
+    request
+      .delete('/carts/587f0f2586653d19297d40c6')
+      .expect(204)
+      .end(done);
+  });
+
+  it('PUT /carts/:cartId', (done) => {
+    const cart = {
+      userId: '9',
+      items: [
+        {
+          count: 4,
+          item: '587f0f2586653d19297d40c2'
+        }
+      ]
+    };
+
+    request
+      .put('/carts/587f0f2586653d19297d40c6')
+      .send(cart)
+      .expect(204)
+      .end(done);
+  });
+});
